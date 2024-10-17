845883c77b25a325725436dca8ad5a53fee48b18
Rafi Shahriar <cm.rafi.shahriar@gmail.com>
2024-10-17 14:47:36 +0600

    mild fix

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,5 +1,5 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 
 
 export class LoginDto {
